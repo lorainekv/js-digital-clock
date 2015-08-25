8fd5cc5aec4509284623db0ee558cec355ab9f7d
Loraine <loraine@lorainekv.com>
2015-08-25 14:10:43 -0700

    Created a new div for displaying hawaii time.

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 // This sets up the display
-function displayTime() {
-  var currentTime = new Date();
+function displayTimes() {
+  var adventureTime = new Date();
   // document represents the html page that we're working in.
-  var clock = document.getElementById("clock");
+  var localClock = document.getElementById("local-clock");
+  var hawaiiClock = document.getElementById("hawaii-clock");
   // You don't need to return a value, since this sets the display text
   // within the function
-  clock.innerText = currentTime.toLocaleTimeString();
+  localClock.innerText = adventureTime.toLocaleTimeString();
+  hawaiiClock.innerText = "hawaii time"
 }
 
 // This updates the time each second
 function updateTime(){
-  setInterval(displayTime, 1000);
+  setInterval(displayTimes, 1000);
 }
 
 updateTime();
