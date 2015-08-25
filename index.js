7a18c3709f7ee8454eae85e909f1bcf18041350d
Loraine <loraine@lorainekv.com>
2015-08-25 13:42:24 -0700

    Livecode: Displaying the seconds.

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ function displayTime() {
 
   // document represents the html page that we're working in.
   var clock = document.getElementById("clock");
-  var clockMessage = clock.innerText = "whassap";
-  console.log(clockMessage);
+  // You don't need to return a value, since this sets the display text
+  // within the function
+  clock.innerText = currentTime.getSeconds();
 }
 
 displayTime();
