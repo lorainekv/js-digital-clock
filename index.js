50b803890e130d4af774d46ef99f204ca9655e75
Loraine <loraine@lorainekv.com>
2015-08-25 13:59:51 -0700

    Clarifying comments.

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-// Your code here
-
+// This sets up the display
 function displayTime() {
   var currentTime = new Date();
-  // console.log(currentTime);
-
   // document represents the html page that we're working in.
   var clock = document.getElementById("clock");
   // You don't need to return a value, since this sets the display text
   // within the function
- clock.innerText = currentTime.toLocaleTimeString();
-
+  clock.innerText = currentTime.toLocaleTimeString();
 }
 
+// This updates the time each second
 function updateTime(){
   setInterval(displayTime, 1000);
 }
